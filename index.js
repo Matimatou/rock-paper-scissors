2caf9451468953c2bc9a3ae7c2cc4674cf6c71ab
Mathias <Inutile0@protonmail.com>
2023-06-10 18:53:43 +0200

    add: div for displaying results - FUNCTION displayDivInit : init the div to display the result - change of all console.logs into DOM methods that change div textContent

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const board = document.querySelector('.board');
+const boardParentElement = document.querySelector('body');
 let round = 0;
 const choices = ['Rock', 'Paper', 'Scissors']
 function getComputerChoice() {
@@ -27,17 +29,31 @@ function game(x) {
         round++;
     }
 }
-
 // First we shall get the buttons elements as a nodelist
 const buttonsList = document.querySelectorAll("button");
+
+// Creating the displayDiv
+const resultDiv = document.createElement('div');
+function displayDivInit() {
+    // Create the node element
+    // Add initial text
+    resultDiv.textContent = "No moov played";
+    // Style the node 
+    resultDiv.style.display = "flex";
+    resultDiv.style.margin = "10px";
+    // Append the node
+    boardParentElement.insertBefore(resultDiv, board);
+}
+displayDivInit();
 
 // 
 function play(e) {
     const choice = e.target.getAttribute("data-key");
     const message = playRound(choice, getComputerChoice());
     console.log(message);
-
+    resultDiv.textContent = message;
     round++;
+    return;
 }
 // Then we will apply to all buttons an eventlistener to get the moove that's been played
 buttonsList.forEach((button) => {

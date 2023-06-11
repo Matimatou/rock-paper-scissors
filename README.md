3c9bedfc2934047a998a5013866e902ed64dd54f
Mathias <Inutile0@protonmail.com>
2023-06-11 11:29:10 +0200

    feat: Display score, announce winnner - Display the running score - announce a winner of the game once one player reaches 5 points

--- README.md
+++ README.md
@@ -8,4 +8,4 @@ Reference: [The Odin Project](https://www.theodinproject.com/)
 - Create three buttons, one for each selection. Add an event listener to the buttons that call your playRound function with the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step)
 - Add a div for displaying results and change all of your console.logs into DOM methods.
 - Display the running score, and announce a winner of the game once one player reaches 5 points.
-- You will likely have to refactor (rework/rewrite) your original code to make it work for this. That’s OK! Reworking old code is an important part of the programmer’s life.
+- Refactor (rework/rewrite) original code to make it work for this. 
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+const MAXROUND = 5;
 const board = document.querySelector('.board');
 const boardParentElement = document.querySelector('body');
-let round = 0;
+let round = 1;
 const choices = ['Rock', 'Paper', 'Scissors']
+let score = { player: 0, computer: 0 };
+function scoreToText(score) {
+    return `Player : ${score["player"]} - Computer : ${score["computer"]}`;
+}
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3); // an integer between 0 and 2
     return choices[choice];
@@ -13,48 +18,86 @@ function playRound(playerSelection, computerSelection) {
 
     const result = choices.indexOf(playerSelection) - choices.indexOf(computerSelection);
     if (result === 0) {
-        return `Draw! You both chose ${playerSelection}`;
+        return { message: `Draw! You both chose ${playerSelection}`, winner: "null" };
     } else if (result === 1 || result === -2) {
-        return `You Won! ${playerSelection} beats ${computerSelection}`;
+        return { message: `You Won! ${playerSelection} beats ${computerSelection}`, winner: "player" };
     } else if (result === -1 || result === 2) {
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        return { message: `You Lose! ${computerSelection} beats ${playerSelection}`, winner: "computer" };
     } else {
         return "Invalid selection. Please choose 'rock', 'paper', or 'scissors'.";
     }
 }
 
-function game(x) {
-    let round = 0;
-    while (round < x) {
-        round++;
-    }
-}
+
 // First we shall get the buttons elements as a nodelist
 const buttonsList = document.querySelectorAll("button");
 
 // Creating the displayDiv
-const resultDiv = document.createElement('div');
-function displayDivInit() {
-    // Create the node element
-    // Add initial text
-    resultDiv.textContent = "No moov played";
-    // Style the node 
-    resultDiv.style.display = "flex";
-    resultDiv.style.margin = "10px";
-    // Append the node
-    boardParentElement.insertBefore(resultDiv, board);
-}
-displayDivInit();
+const displayDiv = document.createElement('div');
+// Create the two nodes element
+const resultParagraph = document.createElement('p');
+const scoreParagraph = document.createElement('p');
+function createDisplayDiv() {
+    // Style the main div 
+    displayDiv.style.display = "flex";
+    displayDiv.style.flexDirection = "column";
+    displayDiv.style.alignItems = "center";
+    displayDiv.style.gap = "5px";
 
+    // Style the paragraph to get them closer
+    resultParagraph.style.margin = scoreParagraph.style.margin = "5px";
+    //displayDiv.style.pading = "10px";
+    // Append the node
+    displayDiv.appendChild(scoreParagraph);
+    displayDiv.appendChild(resultParagraph);
+    boardParentElement.insertBefore(displayDiv, board);
+
+}
+function displayDivInit() {
+    // Add initial text
+    resultParagraph.textContent = "No moov played";
+    scoreParagraph.textContent = scoreToText(score);
+}
+function displayDivUpdate(dico) {
+    const winner = dico["winner"]
+    resultParagraph.textContent = dico["message"];
+    if (!winner)
+        return;
+    score[winner]++
+    scoreParagraph.textContent = scoreToText(score);
+}
+function isFinished() {
+    if (score["computer"] == MAXROUND || score["player"] == MAXROUND) {
+        let message;
+        console.log("We entered the dunjeon");
+        if (score["player"] == score["computer"])
+            message = "Draw no one win";
+        else {
+            message = `The winner is ${score["player"] > score["computer"] ? "Player" : "Computer"}`;
+        }
+        console.log(message);
+        resultParagraph.textContent = message;
+        score = { player: 0, computer: 0 };
+        round = 0;
+        return true;
+    }
+    return false;
+}
 // 
 function play(e) {
+    console.log("Round : ", round);
     const choice = e.target.getAttribute("data-key");
-    const message = playRound(choice, getComputerChoice());
-    console.log(message);
-    resultDiv.textContent = message;
+    const dico = playRound(choice, getComputerChoice());
+    console.log(dico["message"]);
+    displayDivUpdate(dico);
     round++;
+    if (isFinished(round))
+        return;
     return;
 }
+createDisplayDiv();
+displayDivInit();
+// Display the running score 
 // Then we will apply to all buttons an eventlistener to get the moove that's been played
 buttonsList.forEach((button) => {
     button.addEventListener("click", play)
